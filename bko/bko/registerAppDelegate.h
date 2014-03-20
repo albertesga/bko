f32e778da17c93e8bc77e741ffe56654ebe19793
Tito Español Gamón <titoermejo@MacBook-Pro-de-Tito.local>
2014-03-20 16:26:10 +0100

    Commiting el primer controlador :)

---
+++ bko/bko/registerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  registerAppDelegate.h
+//  bko
+//
+//  Created by Tito Español Gamón on 20/03/14.
+//  Copyright (c) 2014 bko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface registerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bko/bko/registerViewController.m
@@ -0,0 +1,29 @@
+//
+//  registerViewController.m
+//  bko
+//
+//  Created by Tito Español Gamón on 20/03/14.
+//  Copyright (c) 2014 bko. All rights reserved.
+//
+
+#import "registerViewController.h"
+
+@interface registerViewController ()
+
+@end
+
+@implementation registerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
